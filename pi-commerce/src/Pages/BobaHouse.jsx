26d543b7107456f18ab2368e3be9cde16beac23d
winsonc7 <winsonc@stanford.edu>
2023-11-14 17:19:51 -0800

    added count box

--- pi-commerce/src/Pages/BobaHouse.jsx
+++ pi-commerce/src/Pages/BobaHouse.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BobaHouse.css';
 import backdrop from '../Components/Assets/shop_sign.png'
 import all_product from '../Components/Assets/all_product';
 
 const BobaHouse = () => {
+  // State to keep track of the count of added items
+  const [itemCounts, setItemCounts] = useState(all_product.map(() => 0));
+
+  // Function to handle adding an item
+  const handleAddItem = (index) => {
+    const newCounts = [...itemCounts];
+    newCounts[index] += 1;
+    setItemCounts(newCounts);
+    // Add additional logic here if needed
+  };
+
+  // Function to handle removing an item
+  const handleRemoveItem = (index) => {
+    if (itemCounts[index] > 0) {
+      const newCounts = [...itemCounts];
+      newCounts[index] -= 1;
+      setItemCounts(newCounts);
+      // Add additional logic here if needed
+    }
+  };
+
   return (
   <div>
     <div className="banner">
@@ -14,15 +35,18 @@ const BobaHouse = () => {
     </div>
 
     <div className="product-list">
-    {all_product.map((product) => (
+    {all_product.map((product, index) => (
       <div key={product.id} className="product-item">
         <img src={product.image} alt={product.name} />
         <div>
           <h3>{product.name}</h3>
           <p>{product.price.toFixed(2)} pi</p>
           <div className="action-buttons">
-            <button className="add-button">Add to Cart</button>
-            <button className="remove-button">Remove</button>
+            <button className="add-button" onClick={() => handleAddItem(index)}>Add to Cart</button>
+            <div className="item-count-box">
+              <p>{itemCounts[index]}</p>
+            </div>
+            <button className="remove-button" onClick={() => handleRemoveItem(index)}>Remove</button>
           </div>
         </div>
       </div>
--- pi-commerce/src/Pages/Cart.jsx
+++ pi-commerce/src/Pages/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Cart.css';
 
 const Cart = () => {
   return (
